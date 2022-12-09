f6e73c05f601ec85fefa1e22d57f494e9c269461
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2022-12-08 19:52:10 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export {default as Hero} from './Hero.jsx'; 
+
+const App = () => {
+  return (
+    <>
+      <main><Hero/></main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/hero.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <>
+      <div>
+        <div></div>
+        <div>
+          
+        </div>
+      </div>
+    </>
+  )
+}
