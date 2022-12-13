d951f3b08713370572421133747681bca21e52e9
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2022-12-13 10:03:52 -0600

    changing the item and title

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -4,24 +4,28 @@ import React from 'react'
 const Item = ({id, color, shadow, title, text, img, btn, rating, price}) => {
   return (
     <>
-        <div>
-            <div>
-                <h1>{title}</h1>
-                <p>{text}</p>
-                <div>
-                    <h1>{price}</h1>
+        <div className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center`}>
+            <div className='grid items-center justify-items-center'>
+                <h1 className='text-slate-200 text-xl lg:text-lg md:text-text-base font-medium filter drop-shadow'>{title}</h1>
+                
+                <p className='text-slate-200 filter drop-shadow text-base md:text-sm font-normal'>{text}</p>
+                
+                <div className='flex items-center justify-between w-28'>
+                    <h1 className=''>{price}</h1>
                 </div>
-                <div>
+                
+                <div className='flex items-center justify-between w-28'>
                     <StarIcon className='icon-style text-slate-900'/>
-                    {rating}
+                    <h1 className=''>{rating}</h1>
                 </div>
-                <div>
+                
+                <div className='flex items-center justify-between w-28'>
                     <button type= 'button' className=''> <ShoppingBagIcon className='icon-style text-slate-900'/> </button>
                     <button type='button' className=''> {btn} </button>
                 </div>
             </div>
-            <div>
-                <img src={img} alt='img/item img'/>
+            <div className='flex items-center justify-between w-28'>
+                <img src={img} alt='img/item-img' className=''/>
             </div>
         </div>
     </>
