f17f2454e83bafb20847d891d7ab0a51111a16bb
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2022-12-13 20:13:15 -0600

    pushing current changes

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -10,22 +10,24 @@ const Item = ({id, color, shadow, title, text, img, btn, rating, price}) => {
                 
                 <p className='text-slate-200 filter drop-shadow text-base md:text-sm font-normal'>{text}</p>
                 
-                <div className='flex items-center justify-between w-28'>
-                    <h1 className=''>{price}</h1>
+                <div className='flex items-center justify-between w-28 my-2'>
+                    <div className='flex items-center bg-white/80 px-1 rounded blur-effect-theme'>
+                        <h1 className='text-black text-sm font-medium'>${price}</h1>
+                    </div>
+                    
+                    <div className='flex items-center gap-1'>
+                        <StarIcon className='icon-style w-5 h-5 md:w-4 md:h-4'/>
+                        <h1 className='md:text-sm font-normal text-slate-100'>{rating}</h1>
+                    </div>
                 </div>
                 
-                <div className='flex items-center justify-between w-28'>
-                    <StarIcon className='icon-style text-slate-900'/>
-                    <h1 className=''>{rating}</h1>
-                </div>
-                
-                <div className='flex items-center justify-between w-28'>
-                    <button type= 'button' className=''> <ShoppingBagIcon className='icon-style text-slate-900'/> </button>
-                    <button type='button' className=''> {btn} </button>
+                <div className='flex items-center justify-between w-28 gap-3'>
+                    <button type= 'button' className='bg-white-90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200'> <ShoppingBagIcon className='icon-style text-slate-900'/> </button>
+                    <button type='button' className='bg-white-90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200 px-2 py-1'> {btn} </button>
                 </div>
             </div>
-            <div className='flex items-center justify-between w-28'>
-                <img src={img} alt='img/item-img' className=''/>
+            <div className='flex items-center'>
+                <img src={img} alt='img/item-img' className='h-36 w-64 transitions-theme hover:-rotate-12'/>
             </div>
         </div>
     </>
