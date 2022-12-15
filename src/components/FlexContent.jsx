6f682d3906b023fe51c93f4c937f8edc8820cbf3
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2022-12-15 10:10:06 -0600

    done with sales item styles

---
+++ src/components/FlexContent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const FlexContent = () => {
+  return (
+    <>
+        <div>
+            <div>
+                <h1></h1>
+                <h1></h1>
+                <p></p>
+                <a></a>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default FlexContent
