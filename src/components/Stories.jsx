48ccfb4b02f587ed6332ebe5a27462a1e0695088
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2022-12-17 14:24:34 -0600

    adding stories code

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Title from './utils/Title'
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+// Default theme
+import '@splidejs/react-splide/css';
+import { HeartIcon } from '@heroicons/react/24/solid';
 
 const Stories = ({story: {title, news}}) => {
   return (
     <>
         <div className='nike-container mb-11'>
             <Title title={title} />
+            <div>
+              <Splide>
+                {news.map( (val, i ) => (
+                  <SplideSlide key={i} className="mb=.5">
+                    <div className=''>
+                      <img src={val.img} alt={`img/story${i}`} className=''/>
+                      <div>
+                        <div>
+                          <HeartIcon className='icon-style'/> 
+                          <span>{val.like}</span>
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </SplideSlide>
+                ))}
+              </Splide>
+            </div>
         </div>
     </>
   )
